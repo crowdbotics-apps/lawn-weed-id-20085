8c5e91c70ff3ac54dc8abe493a5394a212072bab
Crowdbotics <team@crowdbotics.com>
2020-09-07 14:19:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lawn_weed_id_20085/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lawn_weed_id_20085;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "lawn_weed_id_20085";
+    }
+}
---
+++ ios/lawn_weed_id_20085Tests/lawn_weed_id_20085Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface lawn_weed_id_20085 : XCTestCase
+
+@end
+
+@implementation lawn_weed_id_20085
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
